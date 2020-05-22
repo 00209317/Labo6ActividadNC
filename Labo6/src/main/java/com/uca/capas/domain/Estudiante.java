9634dd096a568932b317ebb1297e0971a68684b7
Daniel Hernandez <00209317@uca.edu.sv>
2020-05-21 22:45:21 -0600

    añadiendo relaciones

--- Labo6/src/main/java/com/uca/capas/domain/Estudiante.java
+++ Labo6/src/main/java/com/uca/capas/domain/Estudiante.java
@@ -1,10 +1,14 @@
 package com.uca.capas.domain;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
@@ -37,6 +41,9 @@ public class Estudiante {
 	
 	@Column(name = "estado")
 	private Boolean estado;
+	
+	@OneToMany(mappedBy = "estudiante", fetch = FetchType.EAGER)
+	private List<Computadora> computadoras;
 	
 	
 	public Integer getCodigoEstudiante() {
